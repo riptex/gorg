cc2c9e245a7bddfe4e1bf5dbe0217e57eb4f65a5
riptex <dscassa@scassa.com>
2025-08-01 18:09:41 -0500

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,9 +8,8 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site running on App Platform!</p>
-    <p>Now go build something great.</p>
+    <h1>Hey Dorks!</h1>
+    <p>This is akin to a 1990's first website. Please ignore.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
